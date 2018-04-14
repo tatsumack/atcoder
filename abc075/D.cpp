541005aed772cb88bfffe51bd41bdf35fa23c40e
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-04-14 21:09:48 +0900

    tori

---
+++ abc075/D.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+#define int long long
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPS(i,n) for(int i=1, i##_len=(n); i<=i##_len; ++i)
+
+#define INF (3e9)
+
+using namespace std;
+
+signed main()
+{
+
+    return 0;
+}
