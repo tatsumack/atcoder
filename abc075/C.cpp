400d580a228a0667aa02a6d3b2a1d61b57455995
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-04-19 21:28:56 +0900

    abc075

--- abc075/C.cpp
+++ abc075/C.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <stack>
 
 #define int long long
 
@@ -10,8 +11,70 @@
 
 using namespace std;
 
+int N, M;
+int used[51];
+
+struct Edge
+{
+    int from, to;
+    Edge() {};
+    Edge(int _from, int _to) : from(_from), to(_to) {};
+
+};
+Edge edge[51];
+
 signed main()
 {
+    cin >> N >> M;
+    REP(i, M)
+    {
+        int a, b;
+        cin >> a >> b;
+        edge[i] = Edge(a, b);
+    }
+
+    int ans = 0;
+    REP(i, M)
+    {
+        REPS(j, N) used[j] = 0;
+
+        stack<int> s;
+        s.push(1);
+
+        while (!s.empty())
+        {
+            int v = s.top(); s.pop();
+            used[v] = 1;
+            REP(j, M)
+            {
+                if (i == j) continue;
+
+                Edge& e =  edge[j];
+                if (e.from == v)
+                {
+                    if (used[e.to] == 1) continue;
+                    s.push(e.to);
+                }
+                else if (e.to == v)
+                {
+                    if (used[e.from] == 1) continue;
+                    s.push(e.from);
+                }
+            }
+        }
+
+        bool bConnect = true;
+        REPS(j, N)
+        {
+            if (used[j] == 0)
+            {
+                bConnect = false;
+                break;
+            }
+        }
+        if (!bConnect) ++ans;
+    }
+    cout << ans << endl;
 
     return 0;
 }
--- abc075/D.cpp
+++ abc075/D.cpp
@@ -10,8 +10,40 @@
 
 using namespace std;
 
+int N, K;
+int x[51];
+int y[51];
+bool used[51];
+
 signed main()
 {
+    cin >> N >> K;
+    REP(i, N) cin >> x[i] >> y[i];
+
+    int ans = -1;
+    REP (i, N)
+    for (int j = i+1; j < N; ++j)
+    REP (m, N)
+    for (int n = m+1; n < N; ++n)
+    {
+        int x1 = min(x[i], x[j]);
+        int x2 = max(x[i], x[j]);
+        int y1 = min(y[m], y[n]);
+        int y2 = max(y[m], y[n]);
+
+        int num = 0;
+        REP(k, N)
+        {
+            if (x1 <= x[k] && x[k] <= x2 && y1 <= y[k] && y[k] <= y2) ++num;
+        }
+
+        int s = abs(x2 - x1) * abs(y2 - y1);
+        if (num >= K)
+        {
+            ans = ans == -1 ? s : min(ans, s);
+        }
+    }
+    cout << ans << endl;
 
     return 0;
 }
