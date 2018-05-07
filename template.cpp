9edb294475ae7d50adc9a967b096479d09adeb3f
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-05-07 23:05:07 +0900

    modify template

--- template.cpp
+++ template.cpp
@@ -1,38 +1,35 @@
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <deque>
-#include <stack>
-#include <bitset>
 #include <algorithm>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
+#include <bitset>
 #include <cctype>
-#include <string>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
 
 #define int long long
 
 #define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
 #define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
 #define FOR(i, a, b) for (int i = (a), i##_len = (b); i < i##_len; ++i)
-#define REV(i, a) for (int i = (a); i > 0; --i)
+#define REV(i, a) for (int i = (a); i >= 0; --i)
 #define CLR(a) memset((a), 0, sizeof(a))
 #define DUMP(x) cerr << #x << " = " << (x) << endl;
 #define INF (3e15)
 
 using namespace std;
 
-signed main()
-{
-    return 0;
-}
+signed main() { return 0; }
