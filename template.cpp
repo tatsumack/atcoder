be97108514a63b19c6fa70b9df4493347143ecf9
Tatsuhiko Makino <tatsuhiko.makino>
2018-05-22 18:27:06 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -24,8 +24,8 @@
 
 #define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
 #define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
-#define FOR(i, a, b) for (int i = (a), i##_len = (b); i < i##_len; ++i)
-#define REV(i, a) for (int i = (a); i >= 0; --i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
 #define CLR(a) memset((a), 0, sizeof(a))
 #define DUMP(x) cout << #x << " = " << (x) << endl;
 #define INF (3e15)
