e0037868278c9c6578abd838e75d6e858a686fc2
Tatsuhiko Makino <tatsuhiko.makino>
2018-05-29 18:12:38 +0900

    abc052

---
+++ abc052/D.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define int long long
+
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a) memset((a), 0, sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF (3e15)
+
+using namespace std;
+
+int N, A, B, X[100005];
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> A >> B;
+    REP(i, N)
+        cin >> X[i];
+
+    int ans = 0;
+    REP(i, N - 1)
+    {
+        ans += min(A * (X[i + 1] - X[i]), B);
+    }
+    cout << ans << endl;
+    return 0;
+}
