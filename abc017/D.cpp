abb6522b83abfb153f22f94c9fbb44c80a3b439b
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-06-27 23:09:40 +0900

    abc017

--- abc017/D.cpp
+++ abc017/D.cpp
@@ -32,64 +32,41 @@
 
 using namespace std;
 
-int N, M, F[100005], dp[100005];
+int N, M, F[100005], dp[100005], sum[100005];
 int mod = 1e9 + 7;
-
-int fac[100005], rev[100005];
-
-int binpow(int x, int p) {
-    if (p == 0) return 1;
-
-    if (p % 2 == 0)
-        return binpow((x * x) % mod, p / 2);
-    else
-        return (x * binpow(x, p - 1)) % mod;
-}
-
-int nCr(int n, int r) {
-    if (r > n) return 0;
-    return ((fac[n] * rev[r]) % mod * rev[n - r]) % mod;
-}
-
-void precalc(int n) {
-    fac[0] = fac[1] = 1;
-    FOR(i, 2, n + 2)
-    fac[i] = (fac[i - 1] * i) % mod;
-
-    rev[n + 1] = binpow(fac[n + 1], mod - 2) % mod;
-    REV(i, n, 0) { rev[i] = (rev[i + 1] * (i + 1)) % mod; }
-}
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    precalc(100005);
-
     cin >> N >> M;
     REP(i, N) cin >> F[i];
 
-    set<int> s;
-    int r = 0;
-    int ans = 0;
-    c[0] = 1;
-    REP(l, N) {
-        while (r < N && s.count(F[r]) == 0) {
-            s.insert(F[r]);
-            r++;
+    int l = 0;
+    vector<int> L(N + 1, 0);
+    map<int, int> cnt;
+
+    FOR(r, 0, N - 1) {
+        cnt[F[r]]++;
+        while (l + 1 <= r && cnt[F[r]] > 1) {
+            cnt[F[l]]--;
+            l++;
         }
-        int tmp = 0;
-        REP(j, r - l) { tmp = (tmp + nCr(r - l - 1, j)) % mod; }
-        if (l > 0) tmp = (tmp * c[l - 1]) % mod;
-        ans = (ans + tmp) % mod;
-        DUMP(ans)
-
-        c[r - 1] = tmp;
-
-        l == r ? r++ : s.erase(F[l]);
+        L[r + 1] = l;
     }
-    cout << ans << endl;
+
+    dp[0] = 1;
+    sum[0] = 1;
+    FOR(i, 1, N) {
+        if (L[i] > 0) {
+            dp[i] = (sum[i - 1] - sum[L[i] - 1] + mod) % mod;
+        } else {
+            dp[i] = sum[i - 1];
+        }
+        sum[i] = (sum[i - 1] + dp[i]) % mod;
+    }
+    cout << dp[N] << endl;
 
     return 0;
 }
