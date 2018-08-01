c6e781c40cfba7c3c573565de9c868c64dd34332
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-08-01 22:56:14 +0900

    agc001

---
+++ agc001/B.cpp
@@ -0,0 +1,54 @@
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define int long long
+
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF (LLONG_MAX - 1e5)
+
+using namespace std;
+
+int N, X;
+
+int f(int a, int b) {
+    if (a == 0 || b == 0) return 0;
+    if (b < a) swap(a, b);
+    if (b % a == 0) return 2 * (b / a) * a - a;
+    return 2 * (b / a) * a + f(a, b % a);
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> X;
+    cout << N + f(N - X, X) << endl;
+
+    return 0;
+}
