fd7b3f871dff3e32408effcba738ba3d16365e2c
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-08-06 23:17:52 +0900

    mujin2018

--- mujin-pc-2018/E.cpp
+++ mujin-pc-2018/E.cpp
@@ -21,6 +21,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <queue>
 
 #define int long long
 #define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
@@ -33,9 +34,94 @@
 
 using namespace std;
 
+int N, M, K;
+int sx, sy, gx, gy;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+int dist[1005][1005];
+char S[1005][1005];
+int e[4][200005];
+
+typedef pair<int, pair<int, int>> PP;
+
+void dijkstra() {
+    priority_queue<PP, vector<PP>, greater<PP> > que;
+    REP(i, N) REP(j, M) {
+            dist[i][j] = INF;
+        }
+    dist[sx][sy] = 0;
+    que.push(PP(0, {sx, sy}));
+
+    while (!que.empty()) {
+        PP p = que.top();
+        que.pop();
+        int x = p.second.first;
+        int y = p.second.second;
+        if (dist[x][y] < p.first)
+            continue;
+        REP(i, 4) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if (S[nx][ny] == '#') continue;
+
+            int cost = e[i][p.first % K];
+            if (cost < 0) continue;
+            if (dist[nx][ny] > p.first + cost) {
+                dist[nx][ny] = p.first + cost;
+                que.push(PP(dist[nx][ny], {nx, ny}));
+            }
+        }
+    }
+}
+
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    cin >> N >> M >> K;
+
+    string d;
+    cin >> d;
+    d += d;
+
+    REP(i, N) REP(j, M) {
+            cin >> S[i][j];
+            if (S[i][j] == 'S') {
+                sx = i;
+                sy = j;
+            } else if (S[i][j] == 'G') {
+                gx = i;
+                gy = j;
+            }
+        }
+
+    REP(j, 4) e[j][d.size()] = -INF;
+    REV(i, d.size() - 1, 0) {
+        REP(j, 4) e[j][i] = e[j][i + 1] + 1;
+        switch (d[i]) {
+            case 'U':
+                e[0][i] = 1;
+                break;
+            case 'D':
+                e[1][i] = 1;
+                break;
+            case 'L':
+                e[2][i] = 1;
+                break;
+            case 'R':
+                e[3][i] = 1;
+                break;
+        }
+    }
+
+    dijkstra();
+
+    int ans = dist[gx][gy] == INF ? -1 : dist[gx][gy];
+    cout << ans << endl;
+
     return 0;
 }
