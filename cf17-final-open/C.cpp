2ead8414797b0b4128d1aef9a6ad091a376f91a4
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-08-19 13:12:01 +0900

    500

--- cf17-final-open/C.cpp
+++ cf17-final-open/C.cpp
@@ -38,5 +38,86 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+    map<int, int> cnt;
+    cnt[0] = 1;
+    cnt[24] = 1;
+    REP(i, N) {
+        int d;
+        cin >> d;
+        cnt[d]++;
+        cnt[24 - d]++;
+    }
+
+    if (cnt[0] > 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    // 右回り
+    bool useNext = true;
+    vector<int> r;
+    FOR(i, 0, 24) {
+        if (cnt.count(i) == 0) continue;
+        if (cnt[i] >= 3) {
+            cout << 0 << endl;
+            return 0;
+        }
+        if (cnt[i] == 2 || i == 12 || i == 24) {
+            r.push_back(i);
+            continue;
+        }
+        if (cnt[i] == 1) {
+            if (useNext) {
+                r.push_back(i);
+                useNext = false;
+            }
+            else
+            {
+                useNext = true;
+            }
+        }
+    }
+
+    int rans = INF;
+    REP(i, r.size() - 1) {
+        rans = min(rans, abs(r[i+1] - r[i]));
+    }
+
+    // 左回り
+    useNext = true;
+    vector<int> l;
+    REV(i, 24, 0) {
+        if (cnt.count(i) == 0) continue;
+        if (cnt[i] >= 3) {
+            cout << 0 << endl;
+            return 0;
+        }
+        if (cnt[i] == 2 || i == 12 || i == 0) {
+            l.push_back(i);
+            continue;
+        }
+        if (cnt[i] == 1) {
+            if (useNext) {
+                l.push_back(i);
+                useNext = false;
+            }
+            else
+            {
+                useNext = true;
+            }
+        }
+    }
+
+    int lans = INF;
+    REP(i, l.size() - 1) {
+        lans = min(lans, abs(l[i+1] - l[i]));
+    }
+
+    int ans = max(lans, rans);
+    cout << ans << endl;
+
     return 0;
 }
