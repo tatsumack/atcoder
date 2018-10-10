24de778320a2919a55d297fd3a3b222c4c4160a0
Tatsuhiko Makino <tatsu.mack@gmail.com>
2018-10-11 00:29:17 +0900

    agc026

---
+++ agc026/B.cpp
@@ -0,0 +1,68 @@
+
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(10)
+
+using namespace std;
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int A, B, C, D;
+        cin >> A >> B >> C >> D;
+
+        if (D < B) {
+            cout << "No" << endl;
+            continue;
+        }
+        if (A < B)
+        {
+            cout << "No" << endl;
+            continue;
+        }
+        int g = gcd(B, D);
+        if (B - g + A % g > C) {
+            cout << "No" << endl;
+        }
+        else {
+            cout << "Yes" << endl;
+        }
+    }
+    return 0;
+}
