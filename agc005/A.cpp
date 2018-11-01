6f9695ec569860a3a473ceea7c0bf56b1f39d3cb
tatsumack <tatsu.mack@gmail.com>
2018-11-01 22:37:17 +0900

    300

---
+++ agc005/A.cpp
@@ -0,0 +1,61 @@
+
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(10)
+
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string x;
+    cin >> x;
+
+    int s = 0;
+    int num = 0;
+    REP(i, x.size()) {
+        char c = x[i];
+        if (c == 'S') {
+            s++;
+        }
+        else {
+            if (s > 0) {
+                s--;
+                num++;
+            }
+        }
+    }
+    cout << x.size() - num * 2 << endl;
+    return 0;
+}
