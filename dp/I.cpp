36c7ef181a8c769a71b3f63695ab5cde7b9aabf5
tatsumack <tatsu.mack@gmail.com>
2019-01-11 23:35:51 +0900

    dp

---
+++ dp/I.cpp
@@ -0,0 +1,65 @@
+
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(10)
+
+using namespace std;
+
+double dp[3005][3005];
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<double> p(N);
+    REP(i, N) cin >> p[i];
+
+    dp[0][0] = 1;
+    REP(i, N) {
+        REP(j, i + 1) {
+            dp[i + 1][j + 1] += dp[i][j] * p[i];
+            dp[i + 1][j] += dp[i][j] * (1 - p[i]);
+        }
+    }
+
+    double ans = 0;
+    FOR(i, 1, N) {
+        if (i > N / 2) ans += dp[N][i];
+    }
+    fcout << ans << endl;
+
+    return 0;
+}
