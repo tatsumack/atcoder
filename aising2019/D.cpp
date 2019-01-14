128ba716afd6f7957b71129f247fa0127fdc65da
tatsumack <tatsu.mack@gmail.com>
2019-01-15 00:56:22 +0900

    aising2019 D

--- aising2019/D.cpp
+++ aising2019/D.cpp
@@ -38,5 +38,43 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N), X(Q), sum(N + 1), sum2(N + 1);
+    REP(i, N) cin >> A[i];
+    REP(i, Q) cin >> X[i];
+
+    sum2[0] = A[0];
+    sum2[1] = A[1];
+    REP(i, N) {
+        sum[i + 1] = sum[i] + A[i];
+        if (i - 2 >= 0) {
+            sum2[i] = sum2[i - 2] + A[i];
+        }
+    }
+
+    REP(q, Q) {
+        int x = X[q];
+        int ok = N - 1;
+        int ng = 0;
+        while (ok - ng > 1) {
+            int mid = (ok + ng) / 2;
+
+            int upper = N - 1 - mid + 1;
+            int lower = mid - static_cast<int>(lower_bound(A.begin(), A.end(), x - (A[mid] - x)) - A.begin()) + 1;
+            if (upper <= lower) {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        }
+
+        int res = sum[N] - sum[ok];
+        int next = N - 1 - (N - ok) * 2;
+        if (next >= 0) res += sum2[next];
+        cout << res << endl;
+    }
+
     return 0;
 }
