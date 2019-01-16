d50cb8e22dd2129069f6dac03af2e5ab5d3100c5
tatsumack <tatsu.mack@gmail.com>
2019-01-17 00:48:45 +0900

    dp K

---
+++ dp/K.cpp
@@ -0,0 +1,64 @@
+
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(10)
+
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> a(N);
+    REP(i, N) cin >> a[i];
+
+    // Grundy数
+    vector<int> dp(K + 1);
+    dp[0] = 0;
+    FOR(i, 1, K) {
+        set<int> s;
+        REP(j, N) {
+            if (i - a[j] < 0) continue;
+            s.insert(dp[i - a[j]]);
+        }
+        while (s.count(dp[i])) {
+            dp[i]++;
+        }
+    }
+    cout << (dp[K] > 0 ? "First" : "Second") << endl;
+
+    return 0;
+}
