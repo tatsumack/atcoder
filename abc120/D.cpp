13c1c9f39cca014369ca1ea12002f80bca228297
tatsumack <tatsu.mack@gmail.com>
2019-03-03 21:33:45 +0900

    abc120

---
+++ abc120/D.cpp
@@ -0,0 +1,120 @@
+
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(10)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+struct UnionFind {
+    vector<int> par;
+    vector<int> rank;
+    vector<int> vsize;
+
+    UnionFind(int size) : par(size), rank(size), vsize(size) {
+        REP(i, size) {
+            par[i] = i;
+            rank[i] = 0;
+            vsize[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (par[x] == x) {
+            return x;
+        } else {
+            return par[x] = find(par[x]);
+        }
+    }
+
+    void unite(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) return;
+        if (rank[x] < rank[y]) {
+            par[x] = y;
+        } else {
+            par[y] = x;
+            if (rank[x] == rank[y]) rank[x]++;
+        }
+        vsize[x] += vsize[y];
+        vsize[y] = vsize[x];
+    }
+
+    bool same(int x, int y) { return find(x) == find(y); }
+
+    int getSize(int x) { return vsize[find(x)]; }
+};
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> A(M), B(M);
+    REP(i, M) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        A[i] = a;
+        B[i] = b;
+    }
+
+    vector<int> ans;
+    UnionFind uf(N + 1);
+    REV(i, M - 1, 0) {
+        int a = A[i];
+        int b = B[i];
+
+        if (uf.same(a, b)) {
+            ans.push_back(0);
+       } else {
+            int asize = uf.getSize(a);
+            int bsize = uf.getSize(b);
+            ans.push_back(asize * bsize);
+        }
+        uf.unite(a, b);
+    }
+
+    int sum = 0;
+    REV(i, M - 1, 0) {
+        sum += ans[i];
+        cout << sum << endl;
+    }
+
+    return 0;
+}
