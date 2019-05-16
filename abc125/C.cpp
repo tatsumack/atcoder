84c36aebc6477917962d782aa6f134e8fcb1fb86
tatsumack <tatsu.mack@gmail.com>
2019-05-17 00:12:49 +0900

    abc125 C

---
+++ abc125/C.cpp
@@ -0,0 +1,94 @@
+
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(10)
+
+using namespace std;
+
+template<typename T>
+vector<T> make_v(size_t a) { return vector<T>(a); }
+
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+    return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+    for (auto& e:t) fill_v(e, v);
+}
+
+typedef pair<int, int> P;
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    REP(i, N) cin >> A[i];
+
+    vector<int> l(N + 1), r(N + 1);
+    REP(i, N) {
+        l[i + 1] = gcd(l[i], A[i]);
+    }
+    REV(i, N - 1, 1) {
+        r[i - 1] = gcd(r[i], A[i]);
+    }
+
+    int res = -1;
+    REP(i, N) {
+        if (i == 0) {
+            res = max(res, r[i]);
+        } else if (i == N - 1) {
+            res = max(res, l[i]);
+        } else {
+            res = max(res, gcd(l[i], r[i]));
+        }
+    }
+    cout << res << endl;
+
+
+    return 0;
+}
