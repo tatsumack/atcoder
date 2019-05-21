bb3a52f0d73a17b3245c4ff248b2837d912abba1
tatsumack <tatsu.mack@gmail.com>
2019-05-21 16:11:35 +0900

    abc122 C

---
+++ abc122/C.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+template<typename T>
+vector<T> make_v(size_t a) { return vector<T>(a); }
+
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+    return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+    for (auto& e:t) fill_v(e, v);
+}
+
+typedef pair<int, int> P;
+
+class CGeTAC {
+public:
+    void solve(std::istream& in, std::ostream& out) {
+        int N, Q;
+        in >> N >> Q;
+        string S;
+        in >> S;
+
+        vector<int> sum(N + 1);
+        FOR(i, 1, N - 1) {
+            if (S[i] == 'C' && S[i - 1] == 'A') {
+                sum[i + 1] = sum[i] + 1;
+            } else {
+                sum[i + 1] = sum[i];
+            }
+        }
+
+        while (Q--) {
+            int l, r;
+            in >> l >> r;
+            int res = sum[r] - sum[l - 1];
+            if (l > 1 && S[l-1] == 'C' && S[l-2] == 'A') {
+                res--;
+            }
+            out << res << endl;
+        }
+
+    }
+};
