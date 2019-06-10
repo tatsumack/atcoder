2de98ec2ac22331d1d7c5f3e33a7320e5999405f
tatsumack <tatsu.mack@gmail.com>
2019-06-10 23:31:58 +0900

    dp E

--- dp/E.cpp
+++ dp/E.cpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 #include <limits.h>
 #include <algorithm>
 #include <bitset>
@@ -21,6 +21,9 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
 
 #define int long long
 #define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
@@ -30,44 +33,65 @@
 #define CLR(a, b) memset((a), (b), sizeof(a))
 #define DUMP(x) cout << #x << " = " << (x) << endl;
 #define INF 1001001001001001001ll
-#define fcout cout << fixed << setprecision(10)
+#define fcout cout << fixed << setprecision(12)
 
 using namespace std;
 
-int dp[105][100005];
+typedef pair<int, int> P;
 
-int MAXV = 100000;
+int mod = 1e9+7;
+struct mint {
+    unsigned x; mint() : x(0) { }
+    mint(signed sig) { x = sig < 0 ? sig % mod + mod : sig % mod; }
+    mint(signed long long sig) { x = sig < 0 ? sig % mod + mod : sig % mod; }
+    int get() const { return (int)x; }
+    mint &operator+=(mint that) { if ((x += that.x) >= mod) x -= mod; return *this; }
+    mint &operator-=(mint that) { if ((x += mod - that.x) >= mod) x -= mod; return *this; }
+    mint &operator*=(mint that) { x = (unsigned long long)x * that.x % mod; return *this; }
+    mint &operator/=(mint that) { return *this *= that.inverse(); }
+    mint operator+(mint that) const { return mint(*this) += that; }
+    mint operator-(mint that) const { return mint(*this) -= that; }
+    mint operator*(mint that) const { return mint(*this) *= that; }
+    mint operator/(mint that) const { return mint(*this) /= that; }
+    mint inverse() const {
+        long long a = x, b = mod, u = 1, v = 0;
+        while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+        return mint(u);
+    }
+    bool operator==(mint that) const { return x == that.x; }
+    bool operator!=(mint that) const { return x != that.x; }
+    mint operator-() const { mint t; t.x = x == 0 ? 0 : mod - x; return t; }
+};
 
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+mint dp[100005][2];
 
-    int N, W;
-    cin >> N >> W;
+class ESumEqualsXor {
+public:
+    static constexpr int kStressIterations = 0;
+    static void generateTest(std::ostream& test) {
+    }
 
-    vector<int> w(N), v(N);
-    REP(i, N) cin >> w[i] >> v[i];
+    void solve(std::istream& cin, std::ostream& cout) {
+        string L;
+        cin >> L;
 
-    FOR(i, 0, N) FOR(j, 0, MAXV) dp[i][j] = INF;
+        REP(i, L.size() + 1) {
+            dp[i][0] = 0;
+            dp[i][1] = 0;
+        }
 
-    dp[0][0] = 0;
-    REP(i, N) {
-        FOR(j, 0, MAXV) {
-            dp[i + 1][j] = min(dp[i + 1][j], dp[i][j]);
-            if (j + v[i] <= MAXV) {
-                dp[i + 1][j + v[i]] = min(dp[i][j + v[i]], dp[i][j] + w[i]);
+        dp[0][0] = 1;
+        REP(i, L.size()) {
+            if (L[i] == '1') {
+                dp[i+1][0] += dp[i][0] * 2;
+                dp[i+1][1] += dp[i][0];
+                dp[i+1][1] += dp[i][1] * 3;
+            }
+            else {
+                dp[i+1][0] += dp[i][0];
+                dp[i+1][1] += dp[i][1] * 3;
             }
         }
+        cout << mint(dp[L.size()][0] + dp[L.size()][1]).get() << endl;
     }
-
-    int ans = 0;
-    FOR(j, 0, MAXV) {
-        if (dp[N][j] <= W)
-        {
-            ans = j;
-        }
-    }
-    cout << ans << endl;
-    return 0;
-}
+};
