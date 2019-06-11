44bcc41433c6c7a68421b6c337e23b4faba82bec
tatsumack <tatsu.mack@gmail.com>
2019-06-11 22:04:06 +0900

    abc129 F

--- abc129/FTakahashisBasicsInEducationAndLearning.cpp
+++ abc129/FTakahashisBasicsInEducationAndLearning.cpp
@@ -39,6 +39,52 @@ using namespace std;
 
 typedef pair<int, int> P;
 
+int mod;
+
+//　行列累乗
+typedef vector<int> Vec;
+typedef vector<Vec> Mat;
+
+int add(int x, int y) { return (x += y) >= mod ? x - mod : x; }
+
+int mul(int x, int y) { return 1LL * x * y % mod; }
+
+Vec mulMatVec(Mat a, Vec b) {
+    int n = b.size();
+    Vec ret(n, 0);
+    REP(i, n)REP(j, n)ret[i] = add(ret[i], mul(a[i][j], b[j]));
+    return ret;
+}
+
+Vec mulVecMat(Vec a, Mat b) {
+    int n = a.size();
+    Vec ret(n, 0);
+    REP(i, n)REP(j, n) ret[i] = add(ret[i], mul(a[j], b[j][i]));
+    return ret;
+}
+
+Mat mulMatMat(Mat a, Mat b) {
+    int n = a.size();
+    Mat ret(n, Vec(n, 0));
+    REP(i, n)REP(j, n)REP(k, n) ret[i][j] = add(ret[i][j], mul(a[i][k], b[k][j]));
+    return ret;
+}
+
+Mat fastPow(Mat x, int n) {
+    Mat ret(x.size(), Vec(x.size(), 0));
+    REP(i, x.size())ret[i][i] = 1;
+    while (0 < n) {
+        if ((n % 2) == 0) {
+            x = mulMatMat(x, x);
+            n >>= 1;
+        } else {
+            ret = mulMatMat(ret, x);
+            --n;
+        }
+    }
+    return ret;
+}
+
 class FTakahashisBasicsInEducationAndLearning {
 public:
     static constexpr int kStressIterations = 0;
@@ -47,12 +93,40 @@ public:
     }
 
     void solve(std::istream& cin, std::ostream& cout) {
-        int L, A, B, M;
-        cin >> L >> A >> B >> M;
+        int L, A, B;
+        cin >> L >> A >> B >> mod;
 
-        int res = A % M;
-        res += (B % M) * ((L-1) % M);
-        res %= M;
-        cout << res << endl;
+        vector<int> ten(19);
+        ten[0] = 1;
+        FOR(i, 1, 18) ten[i] = (ten[i - 1] * 10) % mod;
+
+        vector<int> cnt(19);
+        int d = 1;
+        FOR(i, 1, 18) {
+            int next = d * 10 - 1;
+            if (A <= next) {
+                cnt[i] = min((next - A) / B + 1, L);
+            }
+            d *= 10;
+        }
+
+        Mat m = {
+                {1, 0, 0},
+                {0, 1, 0},
+                {0, 0, 1},
+        };
+        FOR(i, 1, 18) {
+            int num = cnt[i] - cnt[i - 1];
+            Mat mat = {
+                    {ten[i], 0,       0},
+                    {1,      1,       0},
+                    {0,      B % mod, 1},
+            };
+            m = mulMatMat(m, fastPow(mat, num));
+        }
+        Vec v = {0, A % mod, 1};
+        v = mulVecMat(v, m);
+
+        cout << v[0] % mod << endl;
     }
 };
