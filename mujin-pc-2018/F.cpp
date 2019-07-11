9387588c85243e87d69e0c237e0982a6ee8f1a6e
tatsumack <tatsu.mack@gmail.com>
2019-07-11 10:30:04 +0900

    mujin F

---
+++ mujin-pc-2018/F.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+int mod = 998244353;
+
+// nCr
+int fac[200005], rev[200005];
+
+int binpow(int x, int p) {
+    if (p == 0) return 1;
+
+    if (p % 2 == 0)
+        return binpow((x * x) % mod, p / 2);
+    else
+        return (x * binpow(x, p - 1)) % mod;
+}
+
+int nCr(int n, int r) {
+    if (r > n) return 0;
+    return ((fac[n] * rev[r]) % mod * rev[n - r]) % mod;
+}
+
+void precalc(int n) {
+    fac[0] = fac[1] = 1;
+    FOR(i, 2, n + 2)
+        fac[i] = (fac[i - 1] * i) % mod;
+
+    rev[n + 1] = binpow(fac[n + 1], mod - 2) % mod;
+    REV(i, n, 0)
+    { rev[i] = (rev[i + 1] * (i + 1)) % mod; }
+}
+
+
+struct mint {
+    unsigned x;
+
+    mint() : x(0) {}
+
+    mint(signed sig) { x = sig < 0 ? sig % mod + mod : sig % mod; }
+
+    mint(signed long long sig) { x = sig < 0 ? sig % mod + mod : sig % mod; }
+
+    int get() const { return (int) x; }
+
+    mint& operator+=(mint that) {
+        if ((x += that.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint& operator-=(mint that) {
+        if ((x += mod - that.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint& operator*=(mint that) {
+        x = (unsigned long long) x * that.x % mod;
+        return *this;
+    }
+
+    mint& operator/=(mint that) { return *this *= that.inverse(); }
+
+    mint operator+(mint that) const { return mint(*this) += that; }
+
+    mint operator-(mint that) const { return mint(*this) -= that; }
+
+    mint operator*(mint that) const { return mint(*this) *= that; }
+
+    mint operator/(mint that) const { return mint(*this) /= that; }
+
+    mint inverse() const {
+        long long a = x, b = mod, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b;
+            std::swap(a, b);
+            u -= t * v;
+            std::swap(u, v);
+        }
+        return mint(u);
+    }
+
+    bool operator==(mint that) const { return x == that.x; }
+
+    bool operator!=(mint that) const { return x != that.x; }
+
+    mint operator-() const {
+        mint t;
+        t.x = x == 0 ? 0 : mod - x;
+        return t;
+    }
+};
+
+mint dp[1005][1005];
+
+class F {
+public:
+    static constexpr int kStressIterations = 0;
+
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        CLR(dp, 0);
+
+        int N;
+        cin >> N;
+        vector<int> a(N), cnt(N + 1);
+        REP(i, N) {
+            cin >> a[i];
+            cnt[a[i]]++;
+        }
+
+        precalc(2000);
+
+        dp[N + 1][0] = 1;
+        REV(i, N, 1) {
+            FOR(j, 0, N) {
+                int num = j + cnt[i];
+                for (int k = 0; k * i <= num; k++) {
+                    mint tmp = 1;
+                    tmp *= binpow(fac[i], k);
+                    tmp *= fac[k];
+                    mint val = dp[i + 1][j] * mint(nCr(num, k * i)) * fac[k * i] / tmp;
+                    dp[i][num - k * i] += val;
+                }
+            }
+        }
+        cout << dp[1][0].get() << endl;
+    }
+};
