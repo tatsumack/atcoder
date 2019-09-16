8c238d2b9ecb3993f92ba52e75e9f73f5c028dd0
tatsumack <tatsu.mack@gmail.com>
2019-09-16 23:43:54 +0900

    arc068 E

---
+++ arc068/ESnukeLine.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+struct BIT {
+    int n;
+    vector<int> bit; // 1-indexd
+
+    BIT(int sz) {
+        bit.resize(sz + 1);
+        n = sz;
+    }
+
+    int sum(int i) {
+        int s = 0;
+        while (i > 0) {
+            s += bit[i];
+            i -= i & -i;
+        }
+        return s;
+    }
+
+    void add(int i, int x) {
+        while (i <= n) {
+            bit[i] += x;
+            i += i & -i;
+        }
+    }
+};
+
+class ESnukeLine {
+public:
+    static constexpr int kStressIterations = 0;
+
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int N, M;
+        cin >> N >> M;
+
+        vector<vector<P>> v(M + 1);
+        REP(i, N) {
+            int l, r;
+            cin >> l >> r;
+            v[r - l + 1].push_back({l, r});
+        }
+
+        BIT bit(1e5 + 5);
+
+        int num = N;
+        FOR(d, 1, M) {
+            if (d > 1) {
+                for (auto kv : v[d - 1]) {
+                    bit.add(kv.first, 1);
+                    bit.add(kv.second + 1, -1);
+                    num--;
+                }
+            }
+
+            int res = num;
+            int cur = d;
+            while (cur <= M) {
+                res += bit.sum(cur);
+                cur += d;
+            }
+            cout << res << endl;
+        }
+    }
+};
