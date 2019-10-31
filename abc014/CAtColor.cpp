e2a25ee34f60d37e1e9381d9adde587fee93ff48
tatsumack <tatsu.mack@gmail.com>
2019-11-01 01:23:01 +0900

    abc014 C

---
+++ abc014/CAtColor.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+struct BIT {
+    int n;
+    vector<int> bit; // 1-indexd
+
+    BIT(int sz) {
+        bit.resize(sz + 1);
+        n = sz;
+    }
+
+    int sum(int i) {
+        int s = 0;
+        while (i > 0) {
+            s += bit[i];
+            i -= i & -i;
+        }
+        return s;
+    }
+
+    void add(int i, int x) {
+        while (i <= n) {
+            bit[i] += x;
+            i += i & -i;
+        }
+    }
+};
+
+class CAtColor {
+public:
+    static constexpr int kStressIterations = 0;
+
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        REP(i, n) {
+            cin >> a[i] >> b[i];
+            a[i]++, b[i]++;
+        }
+
+        BIT bit(1000005);
+        REP(i, n) {
+            bit.add(a[i], 1);
+            bit.add(b[i] + 1, -1);
+        }
+        int res = 0;
+        REP(i, 1000005) {
+            res = max(res, bit.sum(i));
+        }
+        cout << res << endl;
+    }
+};
