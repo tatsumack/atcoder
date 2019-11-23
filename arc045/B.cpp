2854f6d990a9eb156cf4e42f3a600db9c40a3119
tatsumack <tatsu.mack@gmail.com>
2019-11-23 20:52:29 +0900

    arc045 B

---
+++ arc045/B.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+// 区間最小、区間加算
+struct LazySegmentTree {
+private:
+    int n;
+    vector<int> node, lazy;
+
+public:
+    LazySegmentTree(vector<int> v) {
+        int sz = (int) v.size();
+        n = 1;
+        while (n < sz) n *= 2;
+        node.resize(2 * n - 1);
+        lazy.resize(2 * n - 1, 0);
+
+        for (int i = 0; i < sz; i++) node[i + n - 1] = v[i];
+        for (int i = n - 2; i >= 0; i--) node[i] = min(node[i * 2 + 1], node[i * 2 + 2]);
+    }
+
+    void eval(int k, int l, int r) {
+        if (lazy[k] != 0) {
+            node[k] += lazy[k];
+            if (r - l > 1) {
+                lazy[2 * k + 1] += lazy[k];
+                lazy[2 * k + 2] += lazy[k];
+            }
+            lazy[k] = 0;
+        }
+    }
+
+    void add(int a, int b, int x, int k = 0, int l = 0, int r = -1) {
+        if (r < 0) r = n;
+        eval(k, l, r);
+        if (b <= l || r <= a) return;
+        if (a <= l && r <= b) {
+            lazy[k] += x;
+            eval(k, l, r);
+        } else {
+            add(a, b, x, 2 * k + 1, l, (l + r) / 2);
+            add(a, b, x, 2 * k + 2, (l + r) / 2, r);
+            node[k] = min(node[2 * k + 1], node[2 * k + 2]);
+        }
+    }
+
+    int find(int a, int b, int k = 0, int l = 0, int r = -1) {
+        if (r < 0) r = n;
+        eval(k, l, r);
+        if (b <= l || r <= a) return INF;
+        if (a <= l && r <= b) return node[k];
+        int vl = find(a, b, 2 * k + 1, l, (l + r) / 2);
+        int vr = find(a, b, 2 * k + 2, (l + r) / 2, r);
+        return min(vl, vr);
+    }
+};
+
+class B {
+public:
+    static constexpr int kStressIterations = 0;
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int N, M;
+        cin >> N >> M;
+
+        vector<P> st(M);
+        REP(i, M) {
+            int s, t;
+            cin >> s >> t;
+            s--;
+            st[i] = {s, t};
+        }
+
+        vector<int> v(N);
+        LazySegmentTree seg(v);
+        REP(i, M) {
+            int s, t;
+            tie(s, t) = st[i];
+            seg.add(s, t, 1);
+        }
+
+        vector<int> res;
+        REP(i, M) {
+            int s, t;
+            tie(s, t) = st[i];
+            seg.add(s, t, -1);
+            int val = seg.find(0, N);
+            if (val >= 1) {
+                res.push_back(i + 1);
+            }
+            seg.add(s, t, 1);
+        }
+
+        cout << res.size() << endl;
+        for (auto i : res) {
+            cout << i << endl;
+        }
+
+    }
+};
