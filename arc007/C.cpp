a17f532a394c180c7999bd5e3786b9b5de8359ae
tatsumack <tatsu.mack@gmail.com>
2019-11-30 11:56:31 +0900

    arc007 C

---
+++ arc007/C.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+class C {
+public:
+    static constexpr int kStressIterations = 0;
+
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        string s;
+        cin >> s;
+
+        int n = s.size();
+
+        int res = INF;
+        REP(i, 1 << n) {
+            int tmp = 0;
+            int cnt = 0;
+            REP(j, n) {
+                if ((i >> j & 1) == 0) continue;
+                REP(k, n) {
+                    int p = s[k] == 'o' ? 1 << ((j + k) % n) : 0;
+                    tmp |= p;
+                }
+                cnt++;
+            }
+            if (tmp == (1 << n) - 1) {
+                res = min(cnt, res);
+            }
+        }
+        cout << res << endl;
+    }
+};
