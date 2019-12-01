17bfe09fe9f529501bde9f62ced2cb34a436e9bc
tatsumack <tatsu.mack@gmail.com>
2019-12-01 20:36:44 +0900

    arc056 B

---
+++ arc056/B.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+struct UnionFind {
+    vector<int> par;
+    vector<int> rank;
+    vector<int> vsize;
+    UnionFind(int size) : par(size), rank(size), vsize(size) {
+        REP(i, size) {
+            par[i] = i;
+            rank[i] = 0;
+            vsize[i] = 1;
+        }
+    }
+    int find(int x) {
+        if (par[x] == x) {
+            return x;
+        } else {
+            return par[x] = find(par[x]);
+        }
+    }
+    void unite(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) return;
+        if (rank[x] < rank[y]) {
+            par[x] = y;
+        } else {
+            par[y] = x;
+            if (rank[x] == rank[y]) rank[x]++;
+        }
+        vsize[x] += vsize[y];
+        vsize[y] = vsize[x];
+    }
+    bool same(int x, int y) { return find(x) == find(y); }
+    int size(int x) { return vsize[find(x)]; }
+};
+
+class B {
+public:
+    static constexpr int kStressIterations = 0;
+
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int n, m, s;
+        cin >> n >> m >> s;
+        s--;
+
+        vector<P> edges;
+        REP(i, m) {
+            int u, v;
+            cin >> u >> v;
+            u--, v--;
+            if (u > v) swap(u, v);
+            edges.emplace_back(u, v);
+        }
+        sort(edges.rbegin(), edges.rend());
+
+        UnionFind uf(n);
+        int cur = 0;
+        vector<int> res;
+        REV(i, n - 1, 0) {
+            while (cur < m && edges[cur].first >= i) {
+                uf.unite(edges[cur].first, edges[cur].second);
+                cur++;
+            }
+            if (uf.same(s, i)) {
+                res.push_back(i);
+            }
+        }
+
+        sort(res.begin(), res.end());
+        REP(i, res.size()) cout << res[i] + 1 << endl;
+    }
+};
