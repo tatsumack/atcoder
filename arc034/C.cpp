fe9749054b03dc7ceaae4d90f007c8c4ff8326aa
tatsumack <tatsu.mack@gmail.com>
2019-12-10 23:40:08 +0900

    arc034 C

---
+++ arc034/C.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+// 素因数
+map<int, int> getPrimeFactors(int a) {
+    map<int, int> m;
+    for (int i = 2; i * i <= a; i++) {
+        if (a % i != 0) continue;
+        while (a % i == 0) {
+            m[i]++;
+            a /= i;
+        }
+    }
+    if (a > 1) m[a]++;
+    return m;
+}
+
+int mod = 1e9 + 7;
+
+struct mint {
+    unsigned x;
+
+    mint() : x(0) {}
+
+    mint(signed sig) { x = sig < 0 ? sig % mod + mod : sig % mod; }
+
+    mint(signed long long sig) { x = sig < 0 ? sig % mod + mod : sig % mod; }
+
+    int get() const { return (int) x; }
+
+    mint& operator+=(mint that) {
+        if ((x += that.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint& operator-=(mint that) {
+        if ((x += mod - that.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint& operator*=(mint that) {
+        x = (unsigned long long) x * that.x % mod;
+        return *this;
+    }
+
+    mint& operator/=(mint that) { return *this *= that.inverse(); }
+
+    mint operator+(mint that) const { return mint(*this) += that; }
+
+    mint operator-(mint that) const { return mint(*this) -= that; }
+
+    mint operator*(mint that) const { return mint(*this) *= that; }
+
+    mint operator/(mint that) const { return mint(*this) /= that; }
+
+    mint inverse() const {
+        long long a = x, b = mod, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b;
+            std::swap(a, b);
+            u -= t * v;
+            std::swap(u, v);
+        }
+        return mint(u);
+    }
+
+    bool operator==(mint that) const { return x == that.x; }
+
+    bool operator!=(mint that) const { return x != that.x; }
+
+    mint operator-() const {
+        mint t;
+        t.x = x == 0 ? 0 : mod - x;
+        return t;
+    }
+};
+
+class C {
+public:
+    static constexpr int kStressIterations = 0;
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int a, b;
+        cin >> a >> b;
+
+        map<int, int> m;
+        FOR(i, b + 1, a) {
+            auto tmp = getPrimeFactors(i);
+            for (auto kv : tmp) {
+                m[kv.first] += kv.second;
+            }
+        }
+
+        mint res = 1;
+        for (auto kv : m) {
+            res *= kv.second + 1;
+        }
+        cout << res.get() << endl;
+    }
+};
