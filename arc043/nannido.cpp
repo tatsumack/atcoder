0825295602c098e142b146f75b8c944202a41646
tatsumack <tatsu.mack@gmail.com>
2019-12-15 23:01:33 +0900

    arc043 B

---
+++ arc043/nannido.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+int mod = 1e9 + 7;
+
+struct mint {
+    unsigned x;
+
+    mint() : x(0) {}
+
+    mint(signed sig) { x = sig < 0 ? sig % mod + mod : sig % mod; }
+
+    mint(signed long long sig) { x = sig < 0 ? sig % mod + mod : sig % mod; }
+
+    int get() const { return (int) x; }
+
+    mint& operator+=(mint that) {
+        if ((x += that.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint& operator-=(mint that) {
+        if ((x += mod - that.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    mint& operator*=(mint that) {
+        x = (unsigned long long) x * that.x % mod;
+        return *this;
+    }
+
+    mint& operator/=(mint that) { return *this *= that.inverse(); }
+
+    mint operator+(mint that) const { return mint(*this) += that; }
+
+    mint operator-(mint that) const { return mint(*this) -= that; }
+
+    mint operator*(mint that) const { return mint(*this) *= that; }
+
+    mint operator/(mint that) const { return mint(*this) /= that; }
+
+    mint inverse() const {
+        long long a = x, b = mod, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b;
+            std::swap(a, b);
+            u -= t * v;
+            std::swap(u, v);
+        }
+        return mint(u);
+    }
+
+    bool operator==(mint that) const { return x == that.x; }
+
+    bool operator!=(mint that) const { return x != that.x; }
+
+    mint operator-() const {
+        mint t;
+        t.x = x == 0 ? 0 : mod - x;
+        return t;
+    }
+};
+
+struct BIT {
+    int n;
+    vector<int> bit; // 1-indexd
+
+    BIT(int sz) {
+        bit.resize(sz + 1);
+        n = sz;
+    }
+
+    int sum(int i) {
+        int s = 0;
+        while (i > 0) {
+            s += bit[i];
+            i -= i & -i;
+        }
+        return s;
+    }
+
+    void add(int i, int x) {
+        while (i <= n) {
+            bit[i] += x;
+            i += i & -i;
+        }
+    }
+};
+
+class nannido {
+public:
+    static constexpr int kStressIterations = 0;
+
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int n;
+        cin >> n;
+        vector<int> d(n);
+        REP(i, n) cin >> d[i];
+        sort(d.begin(), d.end());
+
+        BIT bit(300005);
+        REP(i, n) {
+            bit.add(d[i], 1);
+        }
+
+        vector<mint> sum(n);
+        REP(i, n) {
+            sum[i] = bit.sum(300001) - bit.sum(d[i] * 2 - 1);
+        }
+        REV(i, n - 2, 0) {
+            sum[i] += sum[i + 1];
+        }
+
+        vector<int> par(n);
+        int l = 0;
+        int r = 0;
+        while (l < n) {
+            while (r + 1 < n && d[l] * 2 > d[r + 1]) {
+                r++;
+            }
+            if (r == n - 1) {
+                par[l] = -1;
+            } else {
+                par[l] = r + 1;
+            }
+
+            if (l == r) r++;
+            l++;
+        }
+
+
+        mint res = 0;
+        REP(i, n) {
+            if (par[i] == -1) continue;
+            res += (mint) bit.sum(d[i] / 2) * sum[par[i]];
+        }
+        cout << res.get() << endl;
+    }
+};
