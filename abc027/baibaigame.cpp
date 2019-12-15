21a3cddf0865229292593465b3a154fe2740fd6c
tatsumack <tatsu.mack@gmail.com>
2019-12-15 22:26:56 +0900

    abc027 C

---
+++ abc027/baibaigame.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define REPS(i, n) for (int i = 1, i##_len = (n); i <= i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+class baibaigame {
+public:
+    static constexpr int kStressIterations = 0;
+    static void generateTest(std::ostream& test) {
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int n;
+        cin >> n;
+
+        int cnt = n;
+        int isTakahashi = 1;
+        while (cnt > 1) {
+            isTakahashi ^= 1;
+            cnt /= 2;
+        }
+
+        int x = 1;
+        int turn = 0;
+        while (x <= n) {
+            if (turn % 2 == 0) {
+                if (isTakahashi) {
+                    x = 2 * x + 1;
+                } else {
+                    x = 2 * x;
+                }
+            } else {
+                if (isTakahashi) {
+                    x = 2 * x;
+                } else {
+                    x = 2 * x + 1;
+                }
+            }
+            turn++;
+        }
+        if (turn % 2 == 0) {
+            cout << "Takahashi" << endl;
+        } else {
+            cout << "Aoki" << endl;
+        }
+    }
+};
