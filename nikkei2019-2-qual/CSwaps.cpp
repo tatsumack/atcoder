0a3d8203d49a76a39b66b9c7dfb93a2f3af979ec
tatsumack <tatsu.mack@gmail.com>
2019-12-17 10:23:46 +0900

    nikkei2019-2-qua C

--- nikkei2019-2-qual/CSwaps.cpp
+++ nikkei2019-2-qual/CSwaps.cpp
@@ -39,42 +39,6 @@ using namespace std;
 
 typedef pair<int, int> P;
 
-// Range Minimum Query
-struct SegmentTree {
-private:
-    int n;
-    vector<int> node;
-
-public:
-    SegmentTree(vector<int> v) {
-        int sz = v.size();
-        n = 1;
-        while (n < sz) n *= 2;
-        node.resize(2 * n - 1, INF);
-        for (int i = 0; i < sz; i++) node[i + n - 1] = v[i];
-        for (int i = n - 2; i >= 0; i--) node[i] = min(node[2 * i + 1], node[2 * i + 2]);
-    }
-
-    void update(int x, int val) {
-        x += (n - 1);
-        node[x] = val;
-        while (x > 0) {
-            x = (x - 1) / 2;
-            node[x] = min(node[2 * x + 1], node[2 * x + 2]);
-        }
-    }
-
-    int getmin(int a, int b, int k = 0, int l = 0, int r = -1) {
-        if (r < 0) r = n;
-        if (r <= a || b <= l) return INF;
-        if (a <= l && r <= b) return node[k];
-
-        int vl = getmin(a, b, 2 * k + 1, l, (l + r) / 2);
-        int vr = getmin(a, b, 2 * k + 2, (l + r) / 2, r);
-        return min(vl, vr);
-    }
-};
-
 class CSwaps {
 public:
     static constexpr int kStressIterations = 0;
@@ -83,62 +47,52 @@ public:
     }
 
     void solve(std::istream& cin, std::ostream& cout) {
-        int N;
-        cin >> N;
-        vector<int> A(N), B(N);
-        REP(i, N) {
-            cin >> A[i];
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        REP(i, n) cin >> a[i];
+        REP(i, n) cin >> b[i];
+
+        vector<P> sorta(n), sortb(n);
+        REP(i, n) sorta[i] = {a[i], i};
+        REP(i, n) sortb[i] = {b[i], i};
+        sort(sorta.begin(), sorta.end());
+        sort(sortb.begin(), sortb.end());
+
+        bool ok = true;
+        REP(i, n) {
+            if (sorta[i].first > sortb[i].first) ok = false;
         }
-        REP(i, N) {
-            cin >> B[i];
+        if (!ok) {
+            cout << "No" << endl;
+            return;
         }
-
-        vector<P> v(N);
-        REP(i, N) v[i] = {B[i], A[i]};
-        sort(v.begin(), v.end());
-
-        vector<int> a, b;
-        for (auto kv : v) {
-            b.push_back(kv.first);
-            a.push_back(kv.second);
-        }
-
-        vector<int> sa = a;
-        sort(sa.begin(), sa.end());
-
-        bool ok = false;
-        REP(i, N) {
-            if (sa[i] > b[i]) {
-                cout << "No" << endl;
-                return;
-            }
-            if (a[i] == sa[i]) {
-                ok = true;
-            }
+        map<int, int> pos;
+        REP(i, n) {
+            pos[sorta[i].second] = sortb[i].second;
         }
 
-        if (ok) {
+        set<int> found;
+        int cur = 0;
+        while (!found.count(cur)) {
+            found.insert(cur);
+            cur = pos[cur];
+        }
+
+        if (found.size() < n) {
             cout << "Yes" << endl;
             return;
         }
 
-        vector<int> vv(N);
-        REP(i, N - 1) {
-            if (sa[i + 1] <= b[i]) {
-                vv[i] = 1;
-            }
+        ok = false;
+        REP(i, n - 1) {
+            if (sorta[i + 1].first <= sortb[i].first) ok = true;
         }
-
-        bool ok2 = false;
-        REP(i, N - 1) {
-            if (sa[i + 1] <= b[i]) {
-                ok2 = true;
-            }
-        }
-        if (!ok2) {
-            cout << "No" << endl;
+        if (ok) {
+            cout << "Yes" << endl;
             return;
+        } else {
+            cout << "No" << endl;
         }
-        cout << "No" << endl;
     }
 };
