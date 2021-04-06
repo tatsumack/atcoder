10dcc4f262d2d74e052322df5d57c0553d1ef078
tatsumack <tatsu.mack@gmail.com>
2021-04-07 00:34:43 +0900

    aising2020 D

--- aising2020/DAnythingGoesToZero.cpp
+++ aising2020/DAnythingGoesToZero.cpp
@@ -11,41 +11,70 @@
 
 using namespace std;
 
-typedef pair<int, int> P;
-
 class DAnythingGoesToZero {
 public:
-    void solve(std::istream& cin, std::ostream& cout) {
-        int N;
-        string X;
-        cin >> N >> X;
-        int num = 0;
-        REP(i, N) {
-            if (X[i] == '1') num++;
-        }
 
-        int n = N;
+    int bitcount(int n) {
+        int cnt = 0;
+        REP(i, 20) {
+            if (n >> i & 1) cnt++;
+        }
+        return cnt;
+    }
+
+    int calc(int x) {
         int res = 0;
-        while (n > 0) {
-            int cur = 0;
-            REP(i, N) {
-                cur = cur << 1;
-                if (X[i] == '1') {
-                    cur |= 1;
-                }
-                if (cur > num) {
-                    cur %= num;
-                }
-            }
-            n = cur;
-            int tmp = 0;
-            REP(i, 60) {
-                if (n >> i & 1) tmp++;
-            }
-            num = tmp;
-            cout << num << endl;
+        while (x > 0) {
+            int num = bitcount(x);
+            x = x % num;
             res++;
         }
-        cout << res << endl;
+        return res;
+    }
+
+    void solve(std::istream& cin, std::ostream& cout) {
+        int N;
+        cin >> N;
+        string X;
+        cin >> X;
+
+        int num = 0;
+        REP(i, N) if (X[i] == '1') num++;
+        vector<int> A(N + 1), B(N + 1);
+        A[0] = B[0] = 1;
+        REP(i, N) {
+            A[i + 1] = (A[i] * 2) % (num + 1);
+            if (num > 1) B[i + 1] = (B[i] * 2) % (num - 1);
+        }
+
+        int a, b;
+        a = b = 0;
+        REP(i, N) {
+            a *= 2;
+            b *= 2;
+            if (X[i] == '1') {
+                a++;
+                b++;
+            }
+            a %= num + 1;
+            if (num > 1) b %= num - 1;
+        }
+
+        REP(i, N) {
+            if (X[i] == '0') {
+                int x = a + A[N - 1 - i];
+                x %= num + 1;
+                cout << calc(x) + 1 << endl;
+            } else {
+                if (num == 1) {
+                    cout << 0 << endl;
+                    continue;
+                }
+                int x = b - B[N - 1 - i];
+                x %= num - 1;
+                if (x < 0) x += num - 1;
+                cout << calc(x) + 1 << endl;
+            }
+        }
     }
 };
