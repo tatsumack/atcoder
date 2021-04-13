53d5525131db9626a30cc115f0420e4a426484f8
tatsumack <tatsu.mack@gmail.com>
2021-04-13 23:50:00 +0900

    agc051 A

---
+++ agc051/ADodecagon.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define int long long
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define FOR(i, a, b) for (int i = (a), i##_len = (b); i <= i##_len; ++i)
+#define REV(i, a, b) for (int i = (a); i >= (b); --i)
+#define CLR(a, b) memset((a), (b), sizeof(a))
+#define DUMP(x) cout << #x << " = " << (x) << endl;
+#define INF 1001001001001001001ll
+#define fcout cout << fixed << setprecision(12)
+
+using namespace std;
+typedef atcoder::modint998244353 mint;
+
+struct combination {
+    vector<mint> fact, ifact;
+
+    combination(int n) : fact(n + 1), ifact(n + 1) {
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
+    }
+
+    mint operator()(int n, int k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+};
+
+class ADodecagon {
+public:
+    void solve(std::istream& cin, std::ostream& cout) {
+        int d;
+        cin >> d;
+        combination cb(2 * d);
+        mint ret = cb(2 * d, d) / 2;
+        cout << ret.val() << endl;
+    }
+};
